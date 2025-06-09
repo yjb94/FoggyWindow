edb7907f3cf95b909e86b3d7e46b973f4444a068
camou <jbyun94@naver.com>
2025-06-09 15:41:23 +0900

    비 두께 및 갯수 조정

--- src/components/Rain/consts.ts
+++ src/components/Rain/consts.ts
@@ -1,10 +1,10 @@
-export const RAIN_COUNT = 50;
+export const RAIN_COUNT = 25;
 
 export const MIN_RAIN_LENGTH = 20;
 export const MAX_RAIN_LENGTH = 40;
 
-export const MIN_RAIN_WIDTH = 1;
-export const MAX_RAIN_WIDTH = 2;
+export const MIN_RAIN_WIDTH = 2;
+export const MAX_RAIN_WIDTH = 3;
 
 export const MIN_RAIN_SPEED = 2500;
 export const MAX_RAIN_SPEED = 3500;
